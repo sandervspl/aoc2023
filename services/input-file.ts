047d4a4b662c1a161f023280f21377b493211e42
Sander <sandervispoel@hotmail.com>
2023-12-01 20:37:34 +0100

    Delete split arg from inputFile

--- services/input-file.ts
+++ services/input-file.ts
@@ -3,12 +3,9 @@ import path from "node:path";
 
 const day = new Date().getDate();
 
-(globalThis as any).inputFile = function inputFile(
-  fileName = "input",
-  split = "\n",
-) {
+(globalThis as any).inputFile = function inputFile(fileName = "input") {
   const file = path.resolve("days", day.toString(), fileName);
   const buffer = fs.readFileSync(file, "utf8");
 
-  return split ? buffer.split(split) : buffer;
+  return buffer;
 };
