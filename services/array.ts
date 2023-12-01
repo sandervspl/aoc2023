3a8ed5b8f5573ebdcd62362ad2959a0225ee9d2f
Sander <sandervispoel@hotmail.com>
2023-12-01 20:37:40 +0100

    Update array.ts

--- services/array.ts
+++ services/array.ts
@@ -1,7 +1,12 @@
 interface Array<T> {
+  first(): T;
   last(): T;
 }
 
-Array.prototype.last = function () {
+Array.prototype.first = function first() {
+  return this[0];
+};
+
+Array.prototype.last = function last() {
   return this[this.length - 1];
 };
